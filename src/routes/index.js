8e3507dfc30d05d1c420952ce1c1b4ab073ef308
masternaip <167619502+masternaip@users.noreply.github.com>
2025-06-22 00:10:24 +0800

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,37 +1,8 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { HomePage, Login, PlanPage, SignUp, WelcomePage } from '../pages';
-import AccountPage from '../pages/AccountPage/AccountPage';
-import MovieList from '../pages/MovieList/MovieList';
-import PageLayout from '../pages/PageLayout/PageLayout';
-import YourAccount from '../pages/YourAccount/YourAccount';
-import PrivateRoute from './privateRoute';
-import PublicRoute from './publicRoute';
-
-const PageRouting = () => {
-  return (
-    <Routes>
-      <Route element={<PublicRoute />}>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup">
-          <Route index element={<SignUp />} />
-          <Route path="plan" element={<PlanPage />} />
-        </Route>
-      </Route>
-      <Route element={<PrivateRoute />}>
-        <Route path="/accounts" element={<AccountPage />} />
-        <Route path="/YourAccount" element={<YourAccount />} />
-        <Route path="/homepage/:id">
-          <Route index element={<PageLayout component={<HomePage />} />} />
-          <Route
-            path=":movietype"
-            element={<PageLayout component={<MovieList />} className={'nav-wrapper'} />}
-          />
-        </Route>
-      </Route>
-    </Routes>
-  );
-};
-
-export default PageRouting;
+<Route path="/login" element={<Login />} />
+<Route path="/signup">
+  <Route index element={<SignUp />} />
+  <Route path="plan" element={<PlanPage />} />
+</Route>
+<Route element={<PrivateRoute />}>
+  ...
+</Route>
