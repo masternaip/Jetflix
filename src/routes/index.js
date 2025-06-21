239f4eddce38d114b4c37e5bdb0b65c10020f883
masternaip <167619502+masternaip@users.noreply.github.com>
2025-06-22 00:23:48 +0800

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,16 @@
-<Route path="/login" element={<Login />} />
+<Route element={<PublicRoute />}>
+  <Route path="/" element={<WelcomePage />} />
+  <Route path="/login" element={<Login />} />
+  <Route path="/signup">
+    <Route index element={<SignUp />} />
+    <Route path="plan" element={<PlanPage />} />
+  </Route>
+</Route>
+<Route element={<PrivateRoute />}>
+  <Route path="/accounts" element={<AccountPage />} />
+  <Route path="/YourAccount" element={<YourAccount />} />
+  <Route path="/homepage/:id">
+    <Route index element={<PageLayout component={<HomePage />} />} />
+    <Route path=":movietype" element={<PageLayout component={<MovieList />} className={'nav-wrapper'} />} />
+  </Route>
+</Route>
