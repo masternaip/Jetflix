da69b5515d3cce954185dce2cd37c5b926639bae
masternaip <167619502+masternaip@users.noreply.github.com>
2025-06-22 00:11:38 +0800

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,1 @@
 <Route path="/login" element={<Login />} />
-<Route path="/signup">
-  <Route index element={<SignUp />} />
-  <Route path="plan" element={<PlanPage />} />
-</Route>
-<Route element={<PrivateRoute />}>
-  ...
-</Route>
