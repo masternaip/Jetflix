4b9f553272b4957e72b31c27639943376d62eb45
masternaip <167619502+masternaip@users.noreply.github.com>
2025-06-22 00:24:10 +0800

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,6 @@
-<Route element={<PublicRoute />}>
-  <Route path="/" element={<WelcomePage />} />
-  <Route path="/login" element={<Login />} />
-  <Route path="/signup">
-    <Route index element={<SignUp />} />
-    <Route path="plan" element={<PlanPage />} />
-  </Route>
-</Route>
-<Route element={<PrivateRoute />}>
-  <Route path="/accounts" element={<AccountPage />} />
-  <Route path="/YourAccount" element={<YourAccount />} />
-  <Route path="/homepage/:id">
-    <Route index element={<PageLayout component={<HomePage />} />} />
-    <Route path=":movietype" element={<PageLayout component={<MovieList />} className={'nav-wrapper'} />} />
-  </Route>
-</Route>
+import HomePage from '../pages/HomePage/HomePage';
+
+<Routes>
+  <Route path="/" element={<HomePage />} />
+  {/* You can add additional public routes here */}
+</Routes>
